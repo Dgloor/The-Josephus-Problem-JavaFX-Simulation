c7c3108a170a4418b53e71f705e5449063f0c744
Mario César Chalén Carvajal <marces312001@hotmail.com>
2020-11-15 18:13:59 -0500

    Avance en clase Soldier

--- josephus_problem/src/main/java/model/Soldier.java
+++ josephus_problem/src/main/java/model/Soldier.java
@@ -5,6 +5,21 @@ package model;
  * @author danny
  */
 public class Soldier {
-    private boolean state;
+    private boolean alive;
     
+    public Soldier(){
+        alive = true;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void die() {
+        alive = false;
+    }
+    
+    public void kill(Soldier s){
+        s.die();
+    }
 }

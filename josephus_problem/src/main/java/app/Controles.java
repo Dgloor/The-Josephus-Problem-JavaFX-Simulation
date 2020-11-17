17ea7d103c010c753ac262ad727119aac6831acd
Danny Loor <danny_loor00@hotmail.com>
2020-11-17 01:53:25 -0500

    Creación clase controles

--- josephus_problem/src/main/java/app/Controles.java
+++ josephus_problem/src/main/java/app/Controles.java
@@ -1,9 +1,72 @@
 package app;
 
+import com.jfoenix.controls.JFXSlider;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.ToggleGroup;
+
 /**
  *
  * @author danny
  */
 public class Controles {
+
+    private ToggleGroup sentido;
+    private JFXSlider amountSlider;
+    private Spinner<Integer> startIndex;
+    private SpinnerValueFactory<Integer> valueFactory;
+    private Button btnStart, btnStop, btnReset;
+
+    public void start() {
+        System.out.println("Start");
+    }
+
+    public void pause() {
+        System.out.println("Pause");
+    }
+
+    public void stop() {
+        System.out.println("Stop");
+    }
+
+    public void reset() {
+        System.out.println("Reset");
+    }
+
+    public void incrementAmount() {
+
+    }
+
+    public void changeStartIndex() {
+    }
+
+    public void setSentido(ToggleGroup sentido) {
+        this.sentido = sentido;
+    }
+
+    public void setAmountSlider(JFXSlider amountSlider) {
+        this.amountSlider = amountSlider;
+    }
+
+    public void setStartIndex(Spinner<Integer> startIndex) {
+        this.startIndex = startIndex;
+         valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 60, 1);
+        startIndex.setValueFactory(valueFactory);
+    }
+
+    public void setBtnStart(Button btnStart) {
+        this.btnStart = btnStart;
+    }
+
+    public void setBtnStop(Button btnStop) {
+        this.btnStop = btnStop;
+    }
+
+    public void setBtnReset(Button btnReset) {
+        this.btnReset = btnReset;
+    }
+    
+    
 
 }
--- josephus_problem/src/main/java/app/baseController.java
+++ josephus_problem/src/main/java/app/baseController.java
@@ -1,59 +1,95 @@
 package app;
 
+import com.jfoenix.controls.JFXSlider;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.shape.Circle;
-import model.DeathCircle;
 
 public class baseController implements Initializable {
 
     @FXML
     BorderPane baseScreen;
     @FXML
-    Circle btnClose;
+    Circle btnClose, btnMinimize;
     @FXML
-    Circle btnMinimize;
+    Button btnStart, btnStop, btnReset, btnMusic;
     @FXML
-    Button btnMusic;
-    
+    AnchorPane circleSpace;
+
+    @FXML
+    ToggleGroup sentido;
+    @FXML
+    JFXSlider amountSlider;
+    @FXML
+    Spinner<Integer> startIndex;
+
     Ventana ventana;
     Audio audio;
+    Controles controles;
+    Simulacion simulacion;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         ventana = new Ventana();
         audio = new Audio();
-     
-        
+        addControls();
     }
 
+    public void addControls() {
+        controles = new Controles();
+        controles.setAmountSlider(amountSlider);
+        controles.setSentido(sentido);
+        controles.setStartIndex(startIndex);
+        controles.setBtnStart(btnStart);
+        controles.setBtnStop(btnStop);
+        controles.setBtnReset(btnReset);
+    }
+
+    @FXML
     public void handlePressed(MouseEvent e) {
         ventana.setPosicion(e);
     }
 
+    @FXML
     public void handleDragged(MouseEvent e) {
         ventana.move(e);
     }
 
+    @FXML
     public void handleMin(MouseEvent e) {
         ventana.minimize();
     }
 
+    @FXML
     public void handleClose(MouseEvent e) {
         ventana.close();
     }
 
+    @FXML
     public void toggleAudioState() {
         audio.switchState();
     }
-    
-//    public void startSimulation(){
-//        controls.start();
-//    }
-    
+
+    @FXML
+    public void startSimulation() {
+        controles.start();
+    }
+
+    @FXML
+    public void stopSimulation() {
+        controles.stop();
+    }
+
+    @FXML
+    public void resetSettings() {
+        controles.reset();
+    }
 }

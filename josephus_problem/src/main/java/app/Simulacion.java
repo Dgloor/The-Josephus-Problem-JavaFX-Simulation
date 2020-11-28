b9855517570310dca42ecade0c878ae4aa0a79b6
Danny Loor <danny_loor00@hotmail.com>
2020-11-27 22:42:28 -0500

    refacto metodo updateCircle

--- josephus_problem/src/main/java/app/Simulacion.java
+++ josephus_problem/src/main/java/app/Simulacion.java
@@ -19,26 +19,26 @@ public class Simulacion {
 
     @FXML
     AnchorPane circleSpace;
-    private int radio = 200;
-    Image img = new Image(App.class.getResourceAsStream("/images/soldado.png"), 50, 50, true, true);
+    private final int radio = 230;
+    Image img;
     private final CircularDoublyLinkedList<Soldier> deathCircle;
-    public final int defaultSize = 30;
+    public final int defaultSize = 20;
     private SimulationState state;
-    
-    
+
     public Simulacion(AnchorPane circleSpace) {
-        deathCircle = new CircularDoublyLinkedList();
         this.circleSpace = circleSpace;
-        this.state = SimulationState.STOPPED;
+        deathCircle = new CircularDoublyLinkedList();
+        state = SimulationState.STOPPED;
+        img = new Image(App.class.getResourceAsStream("/images/soldado.png"), 50, 50, true, true);
         for (int i = 0; i < defaultSize; i++) {
             addSoldier();
         }
-        SetLayout();
     }
 
-    public VBox MakeBox(Image img, double xpos, double ypos) {
+    public VBox makeBox(Image img, double xpos, double ypos) {
         ImageView soldierImg = new ImageView(img);
-        Label lblHead = new Label("" +deathCircle.size()+1 );
+        Label lblHead = new Label("" + deathCircle.size());
+        lblHead.setStyle("-fx-text-fill: white;");
         VBox vbSoldier = new VBox();
         vbSoldier.setLayoutX(xpos);
         vbSoldier.setLayoutY(ypos);
@@ -46,26 +46,26 @@ public class Simulacion {
         return vbSoldier;
     }
 
-    public void SetLayout() {
+    public void updateCircle() {
         for (int i = 0; i < deathCircle.size(); i++) {
-            System.out.println("hagoe sto");
-            double angle = (((double) i) / defaultSize) * 2 * Math.PI;
+            double angle = (((double) i) / deathCircle.size()) * 2 * Math.PI;
             double xpos = radio * Math.cos(angle) + 300;
             double ypos = radio * Math.sin(angle) + 240;
             circleSpace.getChildren().get(i).setLayoutX(xpos);
             circleSpace.getChildren().get(i).setLayoutY(ypos);
         }
-
     }
 
     public void addSoldier() {
         deathCircle.addLast(new Soldier());
-        circleSpace.getChildren().add(MakeBox(img, 300, 240));
+        circleSpace.getChildren().add(makeBox(img, 300, 240));
+        updateCircle();
     }
 
     public void removeSoldier() {
         deathCircle.removeLast();
-        circleSpace.getChildren().remove(deathCircle.size() - 1);
+        circleSpace.getChildren().remove(deathCircle.size());
+        updateCircle();
     }
 
     public void startSimulation() {
@@ -82,16 +82,10 @@ public class Simulacion {
 
     public void updateSoldiersAmount(Integer n) {
         int diff = n - deathCircle.size();
-        System.out.println(deathCircle);
-        for (int i = 0; i < Math.abs(diff); i++) {
-            System.out.println("entro aqui");
-            if (diff > 0) {
-                addSoldier();
-            } else {
-                removeSoldier();
-            }
-            SetLayout();
+        if (diff > 0) {
+            addSoldier();
+        } else {
+            removeSoldier();
         }
-        System.out.println("rnlaetr");
     }
 }
